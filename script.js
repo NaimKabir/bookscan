224d7cc9da0fa619ff070ec79bedbcf1b22c5b6e
NaimKabir <kabir.naim@gmail.com>
2023-08-09 20:10:36 -0700

    the end

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const textToAnimate = "Hello. I am a petty creature who is unthinkingly reactive
 "I have a poor grasp of the difference between neutral and harmful machine learning, and honestly, just seeing the letters 'A' and 'I' next to each other in any context makes me furious.\n" +
 "Instead of engaging with people who point out the flaws in my argument or point out convincing analogue technologies that are clearly fair use of works, I instead elect to tear down a creator even though they took special care to mitigate copyright risks to the writing community and people like me.\n" +
 "I should feel ashamed of myself, but instead I will pat myself on the back because this feels enough like a victory to warrant celebration. Another tragedy like this will happen again in the near future, because I rarely retrospect enough to correct my behavior.\n" + 
-"Yay me!";
+"The End.";
 
 let index = 0;
 
