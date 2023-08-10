69b8d5b3fc11bce9e91b640e6ea0698e92034d1e
NaimKabir <kabir.naim@gmail.com>
2023-08-09 17:46:50 -0700

    soften language — pt 2

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const inputElem = document.getElementById('animatedInput');
 const startBtn = document.getElementById('startBtn');
-const textToAnimate = "Hello. \n" + 
-"I have a very poor grasp of the difference between generative and descriptive machine learning, and honestly, just seeing the letters 'A' and 'I' next to each other in any context makes me furious.\n" +
+const textToAnimate = "Hello. I am frightened by recent advances in technology, and this fear extends even to innocent cases.\n" + 
+"I don't have the background to judge the difference between neutral and harmful machine learning, and sometimes just seeing the letters 'A' and 'I' next to each other in any context makes me furious and defensive.\n" +
 "Instead of engaging with people who point out the flaws in my argument or point out convincing analogue technologies that clearly respect fair use, I instead elect to tear down a creator who took special care to mitigate any such copyright risks to the writing community.\n" +
-"I should feel ashamed of myself, but instead will pat myself on the back because this feels enough like a victory. Yay me!";
+"I should feel ashamed of myself, but instead I will pat myself on the back because this feels enough like a victory. Another tragedy like this will happen again in the near future, because I rarely retrospect enough to correct my behavior.\n" + 
+"The End.";
 
 let index = 0;
 
